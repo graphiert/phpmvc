63ed1877292f0263e7f1577214abdec3df19ee58
Galih. <galihpujiirianto@gmail.com>
2025-01-22 14:35:53 +0700

    Too many "../db.sqlite"

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,14 +12,15 @@ require '../vendor/autoload.php';
 require '../core/App.php';
 
 // Database preparation
+$sqlite_dbname = "../db.sqlite";
 if(
-  !file_exists("../db.sqlite") || file_get_contents("../db.sqlite") == ""
+  !file_exists($sqlite_dbname) || file_get_contents($sqlite_dbname) == ""
 ) {
   http_response_code(500);
   echo "Database file not found or not migrated. Exiting...";
   die();
 }
-App::initDatabase("../db.sqlite");
+App::initDatabase($sqlite_dbname);
 
 // Router initialization
 require '../core/Router.php';
